e2711a6e8547f5a10b6814038fcae53d572cb73f
Ernesto Villalpando Montesinos <ernesto.vm.ws@outlook.com>
2020-07-22 17:04:54 -0500

    Barra de navegacion

--- cfe/src/app/app.module.ts
+++ cfe/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdenComponent } from './orden/orden.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrdenComponent
+    OrdenComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
