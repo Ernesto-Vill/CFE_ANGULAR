e1a4e3810a1b56c686c4dcca03357c8e65767f60
Ernesto Villalpando Montesinos <ernesto.vm.ws@outlook.com>
2020-07-23 12:38:02 -0500

    Correccion

--- cfe/src/app/app.module.ts
+++ cfe/src/app/app.module.ts
@@ -7,20 +7,14 @@ import { OrdenComponent } from './orden/orden.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { OrdenService } from './orden/orden.service';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
+
 import { ClienteComponent } from './cliente/cliente.component';
 import { MaterialComponent } from './material/material.component';
 import{RouterModule,Routes} from '@angular/router';
 import { UsuarioComponent } from './usuario/usuario.component'
-=======
 
-import { ClienteComponent } from './cliente/cliente.component';
 
-import { MaterialComponent } from './material/material.component';
-import{RouterModule,Routes} from '@angular/router';
-import { UsuarioComponent } from './usuario/usuario.component'
 
->>>>>>> 2874d0ae862c672be174620f9148c28125cff58a
 
 const routes: Routes = [
   {path: '',redirectTo:'/material',pathMatch:'full'},
